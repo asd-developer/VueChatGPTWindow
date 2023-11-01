e371d138e6ce992dd79d76aaa4372aaa541ffaf6
Andre Duarte <andre.duarte@frotcom.com>
2023-11-01 00:22:07 +0000

    Header styled with minimize button added

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import VueGPTChatWindow from './components/VueGPTChatWindow.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <VueGPTChatWindow></VueGPTChatWindow>
   </main>
 </template>
 
---
+++ src/components/VueGPTChatWindow.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="chat-container">
+        <header>
+            <img src="gpt_logo.png"
+                 alt="logo">
+            <button onclick="minimizeChat()"><img src="minimize-window.svg"></button>
+        </header>
+
+    </div>
+</template>
+<script setup lang="ts">
+</script>
+<style lang="scss">
+.chat-container {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 30px 1fr 50px;
+  height: 600px;
+  width: 400px;
+  background: red;
+
+  header {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    height: 35px;
+    justify-content: center;
+    align-items: center;
+    background-color: #75ac9d;
+    img {
+      max-width: 100%;
+      width: 35px;
+      height: 35px;
+      margin-left: 5px;
+    }
+    button {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: transparent;
+      border: none;
+      color: white;
+      margin-right: 5px;
+      img {
+        width: 15px;
+        height: 5px;
+      }
+    }
+  }
+}
+</style>
